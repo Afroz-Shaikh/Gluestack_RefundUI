c1f7d8a4d9119fdcfd57d1efc79e20119fdbcff2
Afroz-Shaikh <afrozshaikh725@gmail.com>
2024-03-11 07:36:21 +0530

    Fix: UI Style

--- lib/screens/order_page.dart
+++ lib/screens/order_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:gluestack_ui/gluestack_ui.dart';
 
@@ -6,6 +8,9 @@ class OrderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sw = MediaQuery.of(context).size.width;
+    final mw = sw > 550 ? sw * 0.7 : null;
+    log(mw.toString());
     return Scaffold(
       backgroundColor:
           MediaQuery.of(context).platformBrightness == Brightness.dark
@@ -36,207 +41,233 @@ class OrderPage extends StatelessWidget {
       ),
       bottomNavigationBar: const BottomSubmitButton(),
       body: SingleChildScrollView(
-        child: GSVStack(
-          children: [
-            GSBox(
-                child: GSBox(
-              style: GSStyle(
-                padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                bg: $GSColors.white,
-                dark: GSStyle(bg: const Color(0xff1F2937)),
-                width: double.maxFinite,
-              ),
-              child: GSBox(
+        child: GSBox(
+          style: GSStyle(
+            width: double.maxFinite,
+            color: MediaQuery.of(context).platformBrightness == Brightness.dark
+                ? const Color(0xff374151)
+                : const Color(0xffF5F3FF),
+          ),
+          // color: Colors.white,
+          child: GSVStack(
+            mainAxisAlignment: MainAxisAlignment.center,
+            space: GSVstackSpaces.$lg,
+            children: [
+              GSBox(
                   style: GSStyle(
-                    borderRadius: 5,
-                    padding: const EdgeInsets.all(10),
-                    bg: const Color(0xffF3F4F6),
-                    dark: GSStyle(bg: const Color(0xff374151)),
+                    bg: Colors.white,
+                    dark: GSStyle(bg: const Color(0xff1F2937)),
                   ),
-                  child: GSVStack(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      GSHStack(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GSImage(
-                            fit: BoxFit.cover,
-                            style: GSStyle(
-                              ios: GSStyle(width: 60),
-                              android: GSStyle(width: 60),
-                              margin: EdgeInsets.zero,
-                              padding: const EdgeInsets.all(0),
-                              borderRadius: 5,
-                            ),
-                            imageType: GSImageType.network,
-                            path:
-                                'https://images.unsplash.com/photo-1540331547168-8b63109225b7?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z2lybCUyMG9yYW5nZXxlbnwwfHwwfHx8MA%3D%3D',
+                  child: SizedBox(
+                    width: mw,
+                    child: GSBox(
+                      style: GSStyle(
+                        padding: const EdgeInsets.only(
+                            left: 10, right: 10, top: 10, bottom: 10),
+                        bg: $GSColors.white,
+                        dark: GSStyle(bg: const Color(0xff1F2937)),
+                        width: double.maxFinite,
+                      ),
+                      child: GSBox(
+                          style: GSStyle(
+                            width: mw,
+                            borderRadius: 5,
+                            padding: const EdgeInsets.all(10),
+                            bg: const Color(0xffF3F4F6),
+                            dark: GSStyle(bg: const Color(0xff374151)),
                           ),
-                          const SizedBox(width: 10),
-                          GSVStack(
+                          child: GSVStack(
                             mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                'Sample Text',
-                              ),
-                              GSText(
-                                text: 'Sweater Dress',
-                                style: GSStyle(
-                                    textStyle: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                              ),
-                              GSText(
-                                text: 'Girl self Design',
-                                style: GSStyle(
-                                  textStyle: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w300,
+                              GSHStack(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  GSImage(
+                                    fit: BoxFit.cover,
+                                    style: GSStyle(
+                                      ios: GSStyle(width: 60),
+                                      android: GSStyle(width: 60),
+                                      margin: EdgeInsets.zero,
+                                      padding: const EdgeInsets.all(0),
+                                      borderRadius: 5,
+                                    ),
+                                    imageType: GSImageType.network,
+                                    path:
+                                        'https://images.unsplash.com/photo-1540331547168-8b63109225b7?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z2lybCUyMG9yYW5nZXxlbnwwfHwwfHx8MA%3D%3D',
                                   ),
+                                  const SizedBox(width: 10),
+                                  GSVStack(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const Text(
+                                        'Sample Text',
+                                      ),
+                                      GSText(
+                                        text: 'Sweater Dress',
+                                        style: GSStyle(
+                                            textStyle: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                      ),
+                                      GSText(
+                                        text: 'Girl self Design',
+                                        style: GSStyle(
+                                          textStyle: const TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w300,
+                                          ),
+                                        ),
+                                      ),
+                                      GSText(
+                                        text: '\$1,999',
+                                        style: GSStyle(
+                                            textStyle: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                            ],
+                          )),
+                    ),
+                  )),
+              GSBox(
+                style: GSStyle(
+                  width: mw,
+                  padding: const EdgeInsets.all(10),
+                  bg: Colors.white,
+                  dark: GSStyle(bg: const Color(0xff1F2937)),
+                  // dark: GSStyle(bg: const Color(0xff374151)),
+                ),
+                child: const GSHStack(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GSText(
+                      text: 'Select Reason',
+                    ),
+                    GSIcon(icon: Icons.arrow_forward_ios_rounded)
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+
+              GSBox(
+                style: GSStyle(
+                  width: mw,
+                  padding: const EdgeInsets.all(10),
+                  bg: Colors.white,
+                  dark: GSStyle(bg: const Color(0xff1F2937)),
+                ),
+                child: GSVStack(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const GSText(text: 'Add Photo or Video'),
+                    const SizedBox(height: 10),
+                    GSBox(
+                        style: GSStyle(
+                          color: Colors.black,
+                          // bg: Colors.red,
+                          borderColor: Colors.grey,
+                          borderWidth: 1,
+                          padding: const EdgeInsets.all(10),
+                          borderRadius: 5,
+                          height: 100,
+                        ),
+                        child: GSCenter(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GSIcon(
+                                icon: Icons.cloud_upload_outlined,
+                                style: GSStyle(
+                                  color: Colors.grey,
                                 ),
                               ),
-                              GSText(
-                                text: '\$1,999',
-                                style: GSStyle(
-                                    textStyle: const TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                              ),
+                              const GSText(text: 'Upload')
                             ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                    ],
-                  )),
-            )),
-            GSBox(
-              style: GSStyle(
-                padding: const EdgeInsets.all(10),
-                bg: Colors.white,
-                dark: GSStyle(bg: const Color(0xff1F2937)),
-                // dark: GSStyle(bg: const Color(0xff374151)),
+                          ),
+                        ))
+                  ],
+                ),
               ),
-              child: const GSHStack(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GSText(
-                    text: 'Select Reason',
-                  ),
-                  GSIcon(icon: Icons.arrow_forward_ios_rounded)
-                ],
+              //* Desciption Box
+              GSBox(
+                style: GSStyle(
+                  width: mw,
+                  padding: const EdgeInsets.all(10),
+                  bg: Colors.white,
+                  dark: GSStyle(bg: const Color(0xff1F2937)),
+                ),
+                child: const GSVStack(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GSText(text: 'Description'),
+                    SizedBox(height: 10),
+                    GSTextArea(
+                      hintText: 'Refund Reason (Optional)',
+                    )
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-
-            GSBox(
-              style: GSStyle(
-                padding: const EdgeInsets.all(10),
-                bg: Colors.white,
-                dark: GSStyle(bg: const Color(0xff1F2937)),
+              const SizedBox(
+                height: 10,
               ),
-              child: GSVStack(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const GSText(text: 'Add Photo or Video'),
-                  const SizedBox(height: 10),
-                  GSBox(
-                      style: GSStyle(
-                        color: Colors.black,
-                        // bg: Colors.red,
-                        borderColor: Colors.grey,
-                        borderWidth: 1,
-                        padding: const EdgeInsets.all(10),
-                        borderRadius: 5,
-                        height: 100,
-                      ),
-                      child: GSCenter(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            GSIcon(
-                              icon: Icons.cloud_upload_outlined,
-                              style: GSStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                            const GSText(text: 'Upload')
-                          ],
-                        ),
-                      ))
-                ],
+              GSBox(
+                style: GSStyle(
+                  width: mw,
+                  padding: const EdgeInsets.all(10),
+                  bg: Colors.white,
+                  dark: GSStyle(bg: const Color(0xff1F2937)),
+                ),
+                child: const GSVStack(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GSText(text: 'Refund Amount'),
+                    SizedBox(height: 10),
+                    GSTextArea(
+                      hintText: 'Enter amount you want to be refunded',
+                    )
+                  ],
+                ),
               ),
-            ),
-            //* Desciption Box
-            GSBox(
-              style: GSStyle(
-                padding: const EdgeInsets.all(10),
-                bg: Colors.white,
-                dark: GSStyle(bg: const Color(0xff1F2937)),
+              const SizedBox(
+                height: 10,
               ),
-              child: const GSVStack(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GSText(text: 'Description'),
-                  SizedBox(height: 10),
-                  GSTextArea(
-                    hintText: 'Refund Reason (Optional)',
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            GSBox(
-              style: GSStyle(
-                padding: const EdgeInsets.all(10),
-                bg: Colors.white,
-                dark: GSStyle(bg: const Color(0xff1F2937)),
-              ),
-              child: const GSVStack(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GSText(text: 'Refund Amount'),
-                  SizedBox(height: 10),
-                  GSTextArea(
-                    hintText: 'Enter amount you want to be refunded',
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            GSBox(
-              style: GSStyle(
-                padding: const EdgeInsets.all(10),
-                bg: Colors.white,
-                dark: GSStyle(bg: const Color(0xff1F2937)),
-              ),
-              child: const GSVStack(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GSText(text: 'Email Address'),
-                  SizedBox(height: 10),
-                  GSTextArea(
-                    hintText: 'Enter your email address',
-                  )
-                ],
-              ),
-            )
-          ],
+              GSBox(
+                style: GSStyle(
+                  width: mw,
+                  padding: const EdgeInsets.all(10),
+                  bg: Colors.white,
+                  dark: GSStyle(bg: const Color(0xff1F2937)),
+                ),
+                child: const GSVStack(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GSText(text: 'Email Address'),
+                    SizedBox(height: 10),
+                    GSTextArea(
+                      hintText: 'Enter your email address',
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
